9ca3f52ddddac37010af820d283680dd4c8282bd
Chris <cbenzola@gmail.com>
2021-06-22 14:51:36 -0400

    push

--- php/pitching-server.php
+++ php/pitching-server.php
@@ -1,14 +1,12 @@
 <?php
 
-$id='';
+$id = '';
 
 $db = mysqli_connect('212.1.211.213', 'fanta474', 'giftbag1', 'fanta474_chri');
 
+$id = mysqli_real_escape_string($db, $_GET['playerID']);
 
-$id =  mysqli_real_escape_string($db,$_GET['playerID']);
-
-
-$sql= "SELECT CONCAT(nameFirst, ' ', nameLast) AS name,birthMonth AS month,birthDay AS day,
+$sql = "SELECT CONCAT(nameFirst, ' ', nameLast) AS name,birthMonth AS month,birthDay AS day,
 birthYear AS year, bats, throws FROM Master
 
 where
@@ -20,12 +18,9 @@ $result4 = mysqli_query($db, $sql);
 
 $db2 = mysqli_connect('212.1.211.213', 'fanta474', 'giftbag1', 'fanta474_chri');
 
+$id = mysqli_real_escape_string($db2, $_GET['playerID']);
 
-$id =  mysqli_real_escape_string($db2,$_GET['playerID']);
-
-
-
-	$sql= "SELECT Pitching.yearID, Pitching.teamID, Pitching.W,
+$sql = "SELECT Pitching.yearID, Pitching.teamID, Pitching.W,
 	Pitching.L,Pitching.ERA,Pitching.G,Pitching.GS,Pitching.CG,Pitching.SHO,Pitching.SV,Pitching.H,
 
 	Pitching.ER,Pitching.BB,Pitching.SO, TRIM(LEADING '0' FROM Pitching.BAOpp)
@@ -35,16 +30,15 @@ $id =  mysqli_real_escape_string($db2,$_GET['playerID']);
 
 	where
 Pitching.playerID = '$id'";
-	$result = mysqli_query($db2, $sql);
+$result = mysqli_query($db2, $sql);
 
 /****************************/
 
-  $db3 = mysqli_connect('212.1.211.213', 'fanta474', 'giftbag1', 'fanta474_chri');
+$db3 = mysqli_connect('212.1.211.213', 'fanta474', 'giftbag1', 'fanta474_chri');
 
+$id = mysqli_real_escape_string($db3, $_GET['playerID']);
 
-  $id =  mysqli_real_escape_string($db3,$_GET['playerID']);
-
-		$sql="SELECT COUNT(Pitching.yearID),COUNT(Pitching.teamID),
+$sql = "SELECT COUNT(Pitching.yearID),COUNT(Pitching.teamID),
 SUM(Pitching.W),SUM(Pitching.L),ROUND(avg(Pitching.ERA),2), SUM(Pitching.G),SUM(Pitching.GS), SUM(Pitching.CG),
 SUM(Pitching.SHO),SUM(Pitching.SV),SUM(Pitching.H),SUM(Pitching.ER),SUM(Pitching.BB),
 SUM(Pitching.SO),TRIM(LEADING '0' FROM ROUND(avg(Pitching.BAOpp),3))
@@ -58,12 +52,11 @@ $stmt = mysqli_query($db3, $sql);
 /* Start Awards Query */
 $db4 = mysqli_connect('212.1.211.213', 'fanta474', 'giftbag1', 'fanta474_chri');
 
-
-$id =  mysqli_real_escape_string($db4,$_GET['playerID']);
-$sql= "SELECT yearID, lgID, awardID
+$id = mysqli_real_escape_string($db4, $_GET['playerID']);
+$sql = "SELECT yearID, lgID, awardID
 
 FROM AwardsPlayers INNER JOIN Master ON AwardsPlayers.playerID = Master.playerID
 where AwardsPlayers.playerID = '$id' ";
 
-  $query3 = mysqli_query($db4, $sql);
+$query3 = mysqli_query($db4, $sql);
 /* End Awards Query*/
--- scripts/subScript.js
+++ scripts/subScript.js
@@ -1,64 +1,57 @@
-"use strict";
+$(document).ready(function () {
+  $(".card").click(
+    function () {
+      $(".card").toggleClass("applyflip");
+    }.bind(this)
+  );
 
-$(document).ready(function(){
-
-
-	
-
-	$('.card').click(function(){$('.card').toggleClass('applyflip');}.bind(this));
-
-
-
-	function openTab(evt, playerStat) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("stat_view");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  function openTab(evt, playerStat) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("stat_view");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(playerStat).style.display = "block";
+    evt.currentTarget.className += " active";
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(playerStat).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-
-
-
-
-
-
 });
 
 function setCookie(c_name, value, exdays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value =
+    escape(value) + (exdays == null ? "" : "; expires=" + exdate.toUTCString());
+  document.cookie = c_name + "=" + c_value;
 }
 
 function getCookie(c_name) {
-    var i, x, y, ARRcookies = document.cookie.split(";");
-    for (i = 0; i < ARRcookies.length; i++) {
-        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-        x = x.replace(/^\s+|\s+$/g, "");
-        if (x == c_name) {
-            return unescape(y);
-        }
+  var i,
+    x,
+    y,
+    ARRcookies = document.cookie.split(";");
+  for (i = 0; i < ARRcookies.length; i++) {
+    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+    y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+    x = x.replace(/^\s+|\s+$/g, "");
+    if (x == c_name) {
+      return unescape(y);
     }
+  }
 }
 
 //FEATURED HOVER
-$(document).ready(function(){
-   $(".linkfeat").hover(
-     function () {
-         $(".textfeat").show(500);
-     },
-     function () {
-         $(".textfeat").hide(500);
-     }
- );
+$(document).ready(function () {
+  $(".linkfeat").hover(
+    function () {
+      $(".textfeat").show(500);
+    },
+    function () {
+      $(".textfeat").hide(500);
+    }
+  );
 });
 // JavaScript Document
